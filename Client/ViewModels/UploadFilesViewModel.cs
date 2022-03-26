cbafc7277578cadec6f0e2b25af99c60a9320a1b
se-yoshi <se.yoshi@gmail.com>
2022-03-26 15:11:56 +0900

    コメントの追加

--- Client/ViewModels/UploadFilesViewModel.cs
+++ Client/ViewModels/UploadFilesViewModel.cs
@@ -1,4 +1,4 @@
-using Hnx8.ReadJEnc;
+﻿using Hnx8.ReadJEnc;
 using Microsoft.AspNetCore.Components.Forms;
 using System.Text;
 
@@ -9,10 +9,22 @@ public class UploadFilesViewModel
     private readonly ILogger<UploadFilesViewModel> _logger;
     private readonly HttpClient _httpClient;
 
+    /// <summary>選択されたファイル</summary>
     public IBrowserFile? SelectedFile { get; set; }
+
+    /// <summary>使用可能な文字エンコード一覧</summary>
     public IEnumerable<EncodingInfo> Encodings { get; } = Encoding.GetEncodings().OrderBy(info => info.CodePage).ToArray();
+
+    /// <summary>テキストを読み取る文字エンコードのコードページ</summary>
     public int? EncodingCodePage { get; set; }
+
+    /// <summary>
+    /// ファイルの先頭にあるバイト順序マーク(BOM)を検索するかどうかを示す値<br/>
+    /// テキストを読み取る際にBOMを優先する場合は<c>true</c>、<see cref="EncodingCodePage"/>で指定された文字エンコードを優先する場合は<c>false</c>。
+    /// </summary>
     public bool IsDetectEncodingFromByteOrderMarks { get; set; }
+
+    /// <summary>テキストファイルから取得した文字列</summary>
     public string? Text { get; set; }
 
     public UploadFilesViewModel(ILogger<UploadFilesViewModel> logger, HttpClient httpClient)
@@ -21,6 +33,10 @@ public class UploadFilesViewModel
         _httpClient = httpClient;
     }
 
+    /// <summary>
+    /// <see cref="SelectedFile"/>をアップロードする。
+    /// </summary>
+    /// <returns>非同期操作を表すタスクオブジェクト</returns>
     public async Task UploadSelectedFileAsync()
     {
         if (SelectedFile is null)
@@ -45,13 +61,22 @@ public class UploadFilesViewModel
         multipart.Add(content, "file", SelectedFile.Name);
         var uploadTask = _httpClient.PostAsync("UploadFiles", multipart);
 
-        (Text, EncodingCodePage) = await GetEncodingFromFileAsync(SelectedFile);
+        (EncodingCodePage, Text) = await GetEncodingFromFileAsync(SelectedFile);
 
         var response = await uploadTask;
         _logger.LogDebug("HttpResponseMessage: {Response}", response);
     }
 
-    private static async Task<(string? text, int? codepage)> GetEncodingFromFileAsync(IBrowserFile file)
+    /// <summary>
+    /// ファイルから文字エンコードを自動判別する。
+    /// </summary>
+    /// <param name="file">ファイル</param>
+    /// <returns>
+    /// 非同期操作を表すタスクオブジェクト<br/>
+    /// 処理結果としてファイルの内容から自動判別した文字エンコードのコードページと取得した文字列を返す。<br/>
+    /// バイナリファイルの場合は<c>(null, null)</c>を返す。
+    /// </returns>
+    private static async Task<(int? codepage, string? text)> GetEncodingFromFileAsync(IBrowserFile file)
     {
         byte[] array;
 
@@ -62,18 +87,16 @@ public class UploadFilesViewModel
             array = memory.ToArray();
         }
 
-        var code = ReadJEnc.JP.GetEncoding(array, array.Length, out string readText);
-
-        if (code is null)
-        {
-            return (null, null);
-        }
-        else
-        {
-            return (readText, code.GetEncoding().CodePage);
-        }
+        var code = ReadJEnc.JP.GetEncoding(array, array.Length, out var readText);
+        return (code?.GetEncoding().CodePage, readText);
     }
 
+    /// <summary>
+    /// ファイルからテキストを読み取る。<br/>
+    /// <see cref="SelectedFile"/>から<see cref="EncodingCodePage"/>コードページの文字エンコードでテキストを読み取り、<see cref="Text"/>に設定する。<br/>
+    /// BOMの解釈は<see cref="IsDetectEncodingFromByteOrderMarks"/>の設定に準ずる。
+    /// </summary>
+    /// <returns>非同期操作を表すタスクオブジェクト</returns>
     public async ValueTask ReadFileAsync()
     {
         if (SelectedFile is not null && EncodingCodePage.HasValue)
@@ -84,7 +107,7 @@ public class UploadFilesViewModel
         }
         else
         {
-            Text = string.Empty;
+            Text = null;
         }
     }
 }
